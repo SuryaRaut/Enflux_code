d9b7bc8a209f8038f2abb9df61a1983fe9318d4e
Raut <surya.raut@afs.com>
2023-02-21 21:57:30 -0500

    uncommented array diff code

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # Array Diff
 
-# def array_diff(current, target):
-#     added = []
-#     deleted = []
-#     for i in current:
-#         if i not in target:
-#             deleted.append(i)
-#     for i in target:
-#         if i not in current:
-#             added.append(i)
-#     print(f" additions: {added} \n deletions: {deleted}")
-#
-#
-# l1 = [1, 3, 5, 6, 8, 9]
-# l2 = [1, 2, 5, 7, 9]
-#
-# array_diff(l1, l2)
+def array_diff(current, target):
+    added = []
+    deleted = []
+    for i in current:
+        if i not in target:
+            deleted.append(i)
+    for i in target:
+        if i not in current:
+            added.append(i)
+    print(f" additions: {added} \n deletions: {deleted}")
+
+
+l1 = [1, 3, 5, 6, 8, 9]
+l2 = [1, 2, 5, 7, 9]
+
+array_diff(l1, l2)
 
 
